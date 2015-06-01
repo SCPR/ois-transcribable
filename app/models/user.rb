19d1f538d7dce9b474b14d5f474a13ce49c4c456
Chris Keller <christopherlawrencekeller@gmail.com>
2015-06-01 13:32:30 -0700

    updates to the devise-invitation portion of the app, adds the views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :invitable
+  devise :invitable,
+    :database_authenticatable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+    :invitable,
+    :invite_for => 2.weeks
 end
