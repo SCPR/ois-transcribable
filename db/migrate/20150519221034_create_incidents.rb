118a7cbfc09e10a945c2aec8de8a60f2f7ea3563
Chris Keller <christopherlawrencekeller@gmail.com>
2015-06-02 11:24:29 -0700

    updates to migration statements to fix syntax errors

--- db/migrate/20150519221034_create_incidents.rb
+++ db/migrate/20150519221034_create_incidents.rb
@@ -10,13 +10,12 @@ class CreateIncidents < ActiveRecord::Migration
 
             t.text :district_attorney_conclusion, :narrative_notes, :autopsy_notes, :conclusion_notes, :officer_name_and_badge_number, :officer_police_agency, :what_happened, :underlying_crime_leading_to_shooting, :how_situation_escalated, :victim_case_number, :officer_special_unit, :officer_charges_filed_yes_detail, :victim_charges_filed, null:true
 
-            t.timestamps :created_at, :updated_at,
 
             t.datetime :date_of_incident, :district_attorney_date_of_letter, null: true
 
-            t.boolean :verified, :null:false, :default:false
+            t.boolean :verified, null:false, default:false
 
-            t.boolean multiple_officers, :car_stop, :civillian_armed, :potential_police_video, :potential_civillian_video, :mention_of_waistband_in_report, :fatal, :case_relevant, :victim_arrested, :officer_charges_filed, :officer_self_defense, :officer_defense_of_civillians, :officer_defense_of_officers, :officer_unknown, :officer_serving_warrant, :officer_on_surveillance, :officer_on_undercover, :pursuit_occurred, :victim_gang_member, :victim_ignored_officer_commands, :victim_weapon_recovered, :victim_initiated_physical_threat, :victim_intoxicated, :victim_mentally_ill, :civilian_witnesses, :victim_fired_weapon, :victim_pointed_weapon, :officer_injured, :flag_for_followup, null:true
+            t.boolean :multiple_officers, :car_stop, :civillian_armed, :potential_police_video, :potential_civillian_video, :mention_of_waistband_in_report, :fatal, :case_relevant, :victim_arrested, :officer_charges_filed, :officer_self_defense, :officer_defense_of_civillians, :officer_defense_of_officers, :officer_unknown, :officer_serving_warrant, :officer_on_surveillance, :officer_on_undercover, :pursuit_occurred, :victim_gang_member, :victim_ignored_officer_commands, :victim_weapon_recovered, :victim_initiated_physical_threat, :victim_intoxicated, :victim_mentally_ill, :civilian_witnesses, :victim_fired_weapon, :victim_pointed_weapon, :officer_injured, :flag_for_followup, null:true
 
         end
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,111 +11,134 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601223057) do
+ActiveRecord::Schema.define(version: 20150526222505) do
 
   create_table "incidents", force: :cascade do |t|
-    t.string   "url"
-    t.boolean  "verified",                             default: false, null: false
-    t.string   "civilian_name"
     t.datetime "created_at",                                           null: false
     t.datetime "updated_at",                                           null: false
-    t.string   "district_attorney_file_number",        default: "",    null: false
-    t.string   "district_attorney_county",             default: "",    null: false
-    t.date     "district_attorney_date_of_letter",                     null: false
-    t.text     "district_attorney_conclusion",         
-    t.string   "district_attorney_prepared_report",    default: "",    null: false
-    t.text     "narrative_notes"
-    t.text     "autopsy_notes"
-    t.text     "conclusion_notes"
-    t.boolean  "multiple_officers",                    default: false, null: false
-    t.text     "officer_name_and_badge_number"
-    t.text     "officer_police_agency"
-    t.datetime "date_of_incident"
-    t.text     "what_happened"
-    t.string   "general_location_of_incident"
-    t.string   "type_of_incident"
-    t.text     "underlying_crime_leading_to_shooting"
-    t.text     "how_situation_escalated"
-    t.string   "civillian_weapon"
-    t.boolean  "car_stop",                             default: false, null: false
-    t.boolean  "chase_or_pursuit",                     default: false, null: false
-    t.boolean  "civillian_armed",                      default: false, null: false
-    t.boolean  "potential_police_video",               default: false, null: false
-    t.boolean  "potential_civillian_video",            default: false, null: false
-    t.boolean  "mention_of_waistband_in_report",       default: false, null: false
-    t.boolean  "fatal",                                default: false, null: false
-    t.boolean  "case_relevant",                        default: true,  null: false
-    t.boolean  "victim_arrested",                      default: false, null: false
-    t.text     "victim_case_number"
-    t.text     "officer_special_unit"
-    t.boolean  "officer_charges_filed",                default: false, null: false
-    t.text     "officer_charges_filed_yes_detail"
-    t.boolean  "officer_self_defense",                 default: false, null: false
-    t.boolean  "officer_defense_of_civillians",        default: false, null: false
-    t.boolean  "officer_defense_of_officers",          default: false, null: false
-    t.boolean  "officer_unknown",                      default: false, null: false
-    t.boolean  "officer_serving_warrant",              default: false, null: false
-    t.boolean  "officer_on_surveillance",              default: false, null: false
-    t.boolean  "officer_on_undercover",                default: false, null: false
-    t.boolean  "pursuit_occurred",                     default: false, null: false
-    t.boolean  "victim_gang_member",                   default: false, null: false
-    t.boolean  "victim_ignored_officer_commands",      default: false, null: false
-    t.boolean  "victim_weapon_recovered",              default: false, null: false
-    t.boolean  "victim_initiated_physical_threat",     default: false, null: false
-  end
-
-  create_table "transcriptions", force: :cascade do |t|
+    t.string   "url",                                                  null: false
     t.string   "civilian_name"
-    t.integer  "incident_id"
-    t.string   "user_id"
     t.string   "district_attorney_file_number"
     t.string   "district_attorney_county"
-    t.date     "district_attorney_date_of_letter"
+    t.string   "district_attorney_prepared_report"
+    t.string   "general_location_of_incident"
+    t.string   "type_of_incident"
+    t.string   "civillian_weapon"
+    t.string   "officer_shots_fired"
+    t.string   "da_on_scene"
+    t.string   "da_investigator_on_scene"
+    t.text     "district_attorney_conclusion"
     t.text     "narrative_notes"
     t.text     "autopsy_notes"
     t.text     "conclusion_notes"
-    t.boolean  "multiple_officers"
-    t.datetime "date_of_incident"
-    t.string   "general_location_of_incident"
-    t.string   "type_of_incident"
+    t.text     "officer_name_and_badge_number"
+    t.text     "officer_police_agency"
+    t.text     "what_happened"
     t.text     "underlying_crime_leading_to_shooting"
     t.text     "how_situation_escalated"
-    t.string   "civillian_weapon"
+    t.text     "victim_case_number"
+    t.text     "officer_special_unit"
+    t.text     "officer_charges_filed_yes_detail"
+    t.text     "victim_charges_filed"
+    t.datetime "date_of_incident"
+    t.datetime "district_attorney_date_of_letter"
+    t.boolean  "verified",                             default: false, null: false
+    t.boolean  "multiple_officers"
     t.boolean  "car_stop"
-    t.boolean  "chase_or_pursuit"
     t.boolean  "civillian_armed"
     t.boolean  "potential_police_video"
     t.boolean  "potential_civillian_video"
-    t.text     "district_attorney_conclusion"
-    t.string   "district_attorney_prepared_report"
-    t.text     "officer_name_and_badge_number"
-    t.text     "officer_police_agency"
-    t.text     "what_happened"
     t.boolean  "mention_of_waistband_in_report"
     t.boolean  "fatal"
+    t.boolean  "case_relevant"
+    t.boolean  "victim_arrested"
+    t.boolean  "officer_charges_filed"
+    t.boolean  "officer_self_defense"
+    t.boolean  "officer_defense_of_civillians"
+    t.boolean  "officer_defense_of_officers"
+    t.boolean  "officer_unknown"
+    t.boolean  "officer_serving_warrant"
+    t.boolean  "officer_on_surveillance"
     t.boolean  "officer_on_undercover"
     t.boolean  "pursuit_occurred"
     t.boolean  "victim_gang_member"
     t.boolean  "victim_ignored_officer_commands"
     t.boolean  "victim_weapon_recovered"
+    t.boolean  "victim_initiated_physical_threat"
+    t.boolean  "victim_intoxicated"
+    t.boolean  "victim_mentally_ill"
+    t.boolean  "civilian_witnesses"
+    t.boolean  "victim_fired_weapon"
+    t.boolean  "victim_pointed_weapon"
+    t.boolean  "officer_injured"
+    t.boolean  "flag_for_followup"
+  end
+
+  create_table "transcriptions", force: :cascade do |t|
+    t.datetime "created_at",                                           null: false
+    t.datetime "updated_at",                                           null: false
+    t.string   "civilian_name"
+    t.string   "district_attorney_file_number"
+    t.string   "district_attorney_county"
+    t.string   "district_attorney_prepared_report"
+    t.string   "general_location_of_incident"
+    t.string   "type_of_incident"
+    t.string   "civillian_weapon"
+    t.string   "officer_shots_fired"
+    t.string   "da_on_scene"
+    t.string   "da_investigator_on_scene"
+    t.text     "district_attorney_conclusion"
+    t.text     "narrative_notes"
+    t.text     "autopsy_notes"
+    t.text     "conclusion_notes"
+    t.text     "officer_name_and_badge_number"
+    t.text     "officer_police_agency"
+    t.text     "what_happened"
+    t.text     "underlying_crime_leading_to_shooting"
+    t.text     "how_situation_escalated"
+    t.text     "victim_case_number"
+    t.text     "officer_special_unit"
+    t.text     "officer_charges_filed_yes_detail"
+    t.text     "victim_charges_filed"
+    t.datetime "date_of_incident"
+    t.datetime "district_attorney_date_of_letter"
+    t.boolean  "verified",                             default: false, null: false
+    t.boolean  "multiple_officers"
+    t.boolean  "car_stop"
+    t.boolean  "civillian_armed"
+    t.boolean  "potential_police_video"
+    t.boolean  "potential_civillian_video"
+    t.boolean  "mention_of_waistband_in_report"
+    t.boolean  "fatal"
+    t.boolean  "case_relevant"
+    t.boolean  "victim_arrested"
+    t.boolean  "officer_charges_filed"
     t.boolean  "officer_self_defense"
     t.boolean  "officer_defense_of_civillians"
     t.boolean  "officer_defense_of_officers"
-    t.boolean  "victim_initiated_physical_threat"
-    t.boolean  "case_relevant"
     t.boolean  "officer_unknown"
     t.boolean  "officer_serving_warrant"
     t.boolean  "officer_on_surveillance"
-    t.boolean  "victim_arrested"
-    t.text     "victim_case_number"
-    t.text     "officer_special_unit"
-    t.boolean  "officer_charges_filed"
-    t.text     "officer_charges_filed_yes_detail"
+    t.boolean  "officer_on_undercover"
+    t.boolean  "pursuit_occurred"
+    t.boolean  "victim_gang_member"
+    t.boolean  "victim_ignored_officer_commands"
+    t.boolean  "victim_weapon_recovered"
+    t.boolean  "victim_initiated_physical_threat"
+    t.boolean  "victim_intoxicated"
+    t.boolean  "victim_mentally_ill"
+    t.boolean  "civilian_witnesses"
+    t.boolean  "victim_fired_weapon"
+    t.boolean  "victim_pointed_weapon"
+    t.boolean  "officer_injured"
+    t.boolean  "flag_for_followup"
+    t.integer  "incident_id"
+    t.string   "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: ""
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -126,20 +149,9 @@ ActiveRecord::Schema.define(version: 20150601223057) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "invitation_token"
-    t.datetime "invitation_created_at"
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer  "invitation_limit"
-    t.integer  "invited_by_id"
-    t.string   "invited_by_type"
-    t.integer  "invitations_count",      default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["invitation_token"], name: "index_users_on_invitation_token", unique: true
-  add_index "users", ["invitations_count"], name: "index_users_on_invitations_count"
-  add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
