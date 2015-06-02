479ed52dfc999e1e98394af23bbe70806fd3ff8d
Eric Richardson <e@ericrichardson.com>
2015-06-02 15:00:32 -0400

    Symbolize keys in prod ActionMailer config

--- config/environments/prod.rb
+++ config/environments/prod.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method  = :smtp
-  config.action_mailer.smtp_settings    = Hashie::Mash.new(Rails.application.secrets.email)
+  config.action_mailer.smtp_settings    = Rails.application.secrets.email.symbolize_keys
 
   config.action_mailer.default_url_options = { host: Rails.application.secrets.host }
 end
