2e81cac50362239eb622e0b515b4d8ab290e06be
Chris Keller <christopherlawrencekeller@gmail.com>
2015-06-02 16:05:13 -0700

    adds missing fields from this morning's pre-deploy work

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :incidents, :only => [:index, :show]
   resources :transcriptions, :only => [:new, :create]
   devise_for :users
-  #devise_for :admins
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
