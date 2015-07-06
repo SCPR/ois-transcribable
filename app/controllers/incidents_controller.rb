fd1415b0d22eee36e682bc797046c29716681abd
Chris Keller <christopherlawrencekeller@gmail.com>
2015-07-06 15:59:49 -0700

    adds officer injured count to basic stats page

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -6,6 +6,7 @@ class IncidentsController < ApplicationController
         @transcriptions_count = @transcriptions.count
         @victim_weapon_recovered = @transcriptions.where(victim_weapon_recovered: true).count
         @officer_defense_of_civillians = @transcriptions.where(officer_defense_of_civillians: true).count
+        @officer_injured = @transcriptions.where(officer_injured: true).count
         @victim_intoxicated = @transcriptions.where(victim_intoxicated: true).count
         @victim_mentally_ill = @transcriptions.where(victim_mentally_ill: true).count
         @victim_intoxicated_or_ill = (@victim_intoxicated + @victim_mentally_ill)
