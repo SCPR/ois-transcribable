2533ddf3bfa66021aca28f4346d6e15481fd281f
Chris Keller <christopherlawrencekeller@gmail.com>
2015-12-16 09:55:14 -0800

    attempts to place the hint field for inputs before the input again:

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -12,10 +12,9 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
     b.use :label, class: 'control-label'
-
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     b.use :input, class: 'form-control'
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
   config.wrappers :vertical_file_input, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
@@ -24,20 +23,17 @@ SimpleForm.setup do |config|
     b.optional :maxlength
     b.optional :readonly
     b.use :label, class: 'control-label'
-
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     b.use :input
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
   config.wrappers :vertical_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
-
     b.wrapper tag: 'div', class: 'checkbox' do |ba|
       ba.use :label_input
     end
-
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
     b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
@@ -46,9 +42,9 @@ SimpleForm.setup do |config|
     b.use :html5
     b.optional :readonly
     b.use :label, class: 'control-label'
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     b.use :input
     b.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
   end
 
   config.wrappers :horizontal_form, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
@@ -59,11 +55,10 @@ SimpleForm.setup do |config|
     b.optional :min_max
     b.optional :readonly
     b.use :label, class: 'col-sm-3 control-label'
-
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
       ba.use :input, class: 'form-control'
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
 
@@ -73,23 +68,20 @@ SimpleForm.setup do |config|
     b.optional :maxlength
     b.optional :readonly
     b.use :label, class: 'col-sm-3 control-label'
-
+    b.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
       ba.use :input
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
 
   config.wrappers :horizontal_boolean, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
-
     b.wrapper tag: 'div', class: 'col-sm-offset-3 col-sm-9' do |wr|
       wr.wrapper tag: 'div', class: 'checkbox' do |ba|
         ba.use :label_input, class: 'col-sm-9'
       end
-
       wr.use :error, wrap_with: { tag: 'span', class: 'help-block' }
       wr.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
@@ -98,13 +90,11 @@ SimpleForm.setup do |config|
   config.wrappers :horizontal_radio_and_checkboxes, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
     b.use :html5
     b.optional :readonly
-
     b.use :label, class: 'col-sm-3 control-label'
-
     b.wrapper tag: 'div', class: 'col-sm-9' do |ba|
+      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
       ba.use :input
       ba.use :error, wrap_with: { tag: 'span', class: 'help-block' }
-      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
     end
   end
 
