171fbcf36dcaae0cab5b9e6ac50571b9bf0711ce
Chris Keller <christopherlawrencekeller@gmail.com>
2015-12-16 23:34:12 -0800

    addresses people data controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,7 @@ class HomeController < ApplicationController
     end
 
     def export_dashboard_json_data
-        @people = Person.select("people.id, people.district_attorney_file_number, people.incident_url, people.person_name, people.person_ethnicity, people.person_gender, people.person_age, people.person_killed, people.person_armed, people.armed_with_firearm, people.armed_with_weapon, people.used_vehicle_as_weapon, people.person_ignored_officer_commands, people.person_hid_hands_from_officer, people.person_reached_for_waistband, people.person_fled_by_foot_or_vehicle, people.person_grabbed_for_officers_weapon_holster, people.person_signs_of_mental_illness, people.person_signs_of_impairment").where("on_duty_shooting_case = '1'").joins(:incident).where("district_attorney_county = 'Los Angeles'")
+        @people = Person.select("people.id, people.district_attorney_file_number, people.incident_url, people.person_name, people.person_ethnicity, people.person_gender, people.person_age, people.person_killed, people.person_armed, people.armed_with_firearm, people.armed_with_weapon, people.used_vehicle_as_weapon, people.person_ignored_officer_commands, people.person_hid_hands_from_officer, people.person_reached_for_waistband, people.person_fled_by_foot_or_vehicle, people.person_grabbed_for_officers_weapon_holster, people.person_signs_of_mental_illness, people.person_signs_of_impairment").where("person_shot = '1'").joins(:incident).where("district_attorney_county = 'Los Angeles'").where("on_duty_shooting_case = '1'")
         @people.each do |item|
             # fix the incident date
             date_of_incident_pacific = item.incident.date_of_incident.in_time_zone("Pacific Time (US & Canada)")
@@ -33,7 +33,8 @@ class HomeController < ApplicationController
     end
 
     def export_dashboard_csv_data
-        @people = Person.where("people.on_duty_shooting_case = '1'").joins(:incident).where("district_attorney_county = 'Los Angeles'")
+        @people = Person.where("person_shot = '1'")
+        @these_people = @people.joins(:incident).where("district_attorney_county = 'Los Angeles'").where("on_duty_shooting_case = '1'")
         respond_to do |format|
             format.html
             format.csv do
@@ -44,12 +45,14 @@ class HomeController < ApplicationController
     end
 
     def export_all_json_data
-        @people = Person.where("people.on_duty_shooting_case = '1'").joins(:incident).where("district_attorney_county = 'Los Angeles'")
-        render :json => @people.as_json(:include => :incident)
+        @people = Person.where("person_shot = '1'")
+        @these_people = @people.joins(:incident).where("district_attorney_county = 'Los Angeles'").where("on_duty_shooting_case = '1'")
+        render :json => @these_people.as_json(:include => :incident)
     end
 
     def export_all_csv_data
-        @people = Person.where("people.on_duty_shooting_case = '1'").joins(:incident).where("district_attorney_county = 'Los Angeles'")
+        @people = Person.where("person_shot = '1'")
+        @these_people = @people.joins(:incident).where("district_attorney_county = 'Los Angeles'").where("on_duty_shooting_case = '1'")
         respond_to do |format|
             format.html
             format.csv do
